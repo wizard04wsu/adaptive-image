4c9fe9663aced8541c0d8c8c662dca97b11a13b4
Andy Harrison <wizard04@gmail.com>
2025-04-04 06:39:35 -0400

    test page; fits are WIP

---
+++ test/test.js
@@ -0,0 +1,46 @@
+document.addEventListener('DOMContentLoaded', ()=>{
+	
+	'use strict';
+	
+	const useWidth = document.querySelector('#width');
+	const useHeight = document.querySelector('#height');
+	const widthSlider = document.querySelector('#widthSlider');
+	const heightSlider = document.querySelector('#heightSlider');
+	const alignments = document.querySelector('#alignments');
+	
+	useWidth.addEventListener('change', ()=>{
+		const images = Array.prototype.slice.call(document.querySelectorAll('adaptive-image'), 0);
+		for(const image of images){
+			image.setAttribute('width', useWidth.checked ? widthSlider.value : '');
+		}
+	});
+	widthSlider.addEventListener('input', ()=>{
+		const images = Array.prototype.slice.call(document.querySelectorAll('adaptive-image'), 0);
+		for(const image of images){
+			image.setAttribute('width', useWidth.checked ? widthSlider.value : '');
+		}
+	});
+	
+	useHeight.addEventListener('change', ()=>{
+		const images = Array.prototype.slice.call(document.querySelectorAll('adaptive-image'), 0);
+		for(const image of images){
+			image.setAttribute('height', useHeight.checked ? heightSlider.value : '');
+		}
+	});
+	heightSlider.addEventListener('input', ()=>{
+		const images = Array.prototype.slice.call(document.querySelectorAll('adaptive-image'), 0);
+		for(const image of images){
+			image.setAttribute('height', useHeight.checked ? heightSlider.value : '');
+		}
+	});
+	
+	alignments.addEventListener('change', (event)=>{
+		if(event.target.name === 'alignment'){
+			const images = Array.prototype.slice.call(document.querySelectorAll('adaptive-image'), 0);
+			for(const image of images){
+				image.setAttribute('align', event.target.value);
+			}
+		}
+	});
+	
+});
